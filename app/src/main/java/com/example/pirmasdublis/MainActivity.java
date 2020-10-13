2a804817bba6e6dc50d5a2263526caac145e5cc3
mintautasi <72819321+mintautasi@users.noreply.github.com>
2020-10-13 23:07:14 +0300

    added button to change text label and change color

--- app/src/main/java/com/example/pirmasdublis/MainActivity.java
+++ app/src/main/java/com/example/pirmasdublis/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.pirmasdublis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static com.example.pirmasdublis.R.color.pink;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeTextClick(View view) {
+        this.tvMain.setText("Text was changed");
+       // this.tvMain.setTextColor(pink);
+        tvMain.setTextColor(this.getResources().getColor(R.color.pink));
     }
 }
-g
