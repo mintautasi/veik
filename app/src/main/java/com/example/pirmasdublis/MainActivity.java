1a8fede4280e2ddcee0a6e45287e59d9c7977d17
mintautasi <72819321+mintautasi@users.noreply.github.com>
2020-10-14 00:29:01 +0300

    commitment test

--- app/src/main/java/com/example/pirmasdublis/MainActivity.java
+++ app/src/main/java/com/example/pirmasdublis/MainActivity.java
@@ -1,2 +1,31 @@
+package com.example.pirmasdublis;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static com.example.pirmasdublis.R.color.pink;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeTextClick(View view) {
+        this.tvMain.setText("Text was changed");
+
+
+    }
+
+    public void onbtnChangeTextColorClick(View view) {
+        this.tvMain.setTextColor(this.getResources().getColor(R.color.pink));
+    }
 }
